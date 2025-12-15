dceecc6311fcd4fef39e684b7a4130e03aed7854
yksy97 <152153760+yksy97@users.noreply.github.com>
2025-12-15 13:49:36 +0900

    ログイン画面のルートとコンポーネントの追加

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { Login } from './pages/login/login';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: 'login', component: Login },
+];
